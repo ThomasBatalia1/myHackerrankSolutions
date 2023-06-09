d6a0f25999971858eb05051f0e85585e3b9200a7
ThomasBatalia <bataliats@appstat.edu>
2023-06-09 16:04:04 -0400

    Commented Code for question 3 	modified:   simple-array-sum.py

--- 007simple-array-sum/code/simple-array-sum.py
+++ 007simple-array-sum/code/simple-array-sum.py
@@ -15,11 +15,11 @@ import sys
 
 def simpleArraySum(ar):
     # Write your code here
-    count = 0
-    for i in range (0,len(ar)):
-        count += ar[i]
+    count = 0 #creates a count variable 
+    for i in range (0,len(ar)):  #sets up a for loop to go through each index of the array
+        count += ar[i]  #adds each index of the array to the count
     
-    return count
+    return count  #returns the count
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
