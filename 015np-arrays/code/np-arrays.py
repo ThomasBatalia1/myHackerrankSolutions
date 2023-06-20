322e660f90b8979a863653762c77da9845d96389
ThomasBatalia <bataliats@appstat.edu>
2023-06-20 13:00:42 -0400

    Previous commit neglected exercise 015. 	new file:   015np-arrays/code/np-arrays.py

---
+++ 015np-arrays/code/np-arrays.py
@@ -0,0 +1,9 @@
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    reverse_array = arr[::-1] #uses numpy functionality to reverse the array
+    return numpy.array(reverse_array,dtype='float') #returns the array with all entries as floats
+
+arr = input().strip().split(' ')
