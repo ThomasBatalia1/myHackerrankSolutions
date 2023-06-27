d94c5aff669dc4843aa2b1ea437f1845c99350c7
ThomasBatalia <bataliats@appstat.edu>
2023-06-27 19:44:14 -0400

    Hw5 swap-case update 	modified:   swap-case.py

--- 016swap-case/code/swap-case.py
+++ 016swap-case/code/swap-case.py
@@ -1,5 +1,5 @@
-def swap_case(s):
-    reverse_word = []
+def swap_case(s): #defines the function for swap case
+    reverse_word = [] #sets up array that will be the reversed word
     for i in s: #loops through word and stores letter with flipped capitalization
         reverse_word += i.swapcase() #using the swapcase function
     string = "" #string for storing final string with swapped case
