e2491afe9a2198d72b6454845332d03536a78378
ThomasBatalia <bataliats@appstat.edu>
2023-06-27 19:52:34 -0400

    Hw5 swap-case final 	modified:   code/swap-case.py 	deleted:    ../017python-string-split-and-join/code/python-string-split-and-join.txt

--- 016swap-case/code/swap-case.py
+++ 016swap-case/code/swap-case.py
@@ -4,7 +4,7 @@ def swap_case(s): #defines the function for swap case
         reverse_word += i.swapcase() #using the swapcase function
     string = "" #string for storing final string with swapped case
     for j in reverse_word: #combines each swapped character to form final answer
-        string += str(j)
+        string += str(j)  #adds each entry in reverse_word in the string format to the string
     return string # returns final answer
 
 if __name__ == '__main__':
