1efc8a65f2629421c8779694a0a480d81150e250
ThomasBatalia <bataliats@appstat.edu>
2023-06-27 20:09:06 -0400

    Hw6 final commit 	modified:   python-string-split-and-join.py

--- 017python-string-split-and-join/code/python-string-split-and-join.py
+++ 017python-string-split-and-join/code/python-string-split-and-join.py
@@ -1,5 +1,4 @@
-def split_and_join(line): #defines the function for split and join
-    
+def split_and_join(line): #defines the function for split and join 
     line = line.split(" ") #splits the string into a array, seperates indexes at " "
     line = "-".join(line) #rejoins each index in the array with "-" between each.
     return line #returns the final string
